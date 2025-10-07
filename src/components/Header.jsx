d590e17cd34f5cff4389aa9800cb5ab873900c02
Yeison Andrés López Burbano <andislopez13@gmail.com>
2025-10-06 23:00:26 -0500

    Primera parte del trabajo

---
+++ src/components/Header.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import { Navbar } from "./Navbar";
+
+const Header = () => {
+  const [navOpen, SetNavOpen] = useState(false);
+
+  return (
+    <header className="fixed top-0 left-0 w-full h-20 flex items-center z-40 bg-gradient-to-b from-red-900 to-zinc-900/0">
+      <div className="max-w-screen-2xl w-full mx-auto px-4 md:px-6 flex justify-between items-center">
+        <h1>
+          <a href="/" className="flex items-center gap-2">
+            <img
+              src="/images/favicon.ico"
+              width={40}
+              height={40}
+              alt="Yeison Lopez"
+            />
+          </a>
+        </h1>
+        <div className="relative md:justify-self-center">
+          <button
+            className="menu-btn"
+            onClick={() => SetNavOpen((prev) => !prev)}
+          >
+            <span className="material-symbols-rounded">
+              {navOpen ? "close" : "menu"}
+            </span>
+          </button>
+          <Navbar navOpen={navOpen} />
+        </div>
+        <a href="#contact" className="btn btn-secondary max-md:hidden">
+          Contacto
+        </a>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
